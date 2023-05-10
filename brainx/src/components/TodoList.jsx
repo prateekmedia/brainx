da1e2ab3027b117bc3be6e2c3f5e529a4ebf8cf1
Mihir Pratap Sah <smihirp1@gmail.com>
2023-05-10 17:26:33 +0530

    upd1

--- brainx/src/components/TodoList.jsx
+++ brainx/src/components/TodoList.jsx
@@ -1,12 +1,55 @@
 import React from "react";
 import styled from "styled-components"
+import TodoItem from "./TodoItem";
 
+const primaryColor = "#3ad4b0";
 const TodoListStyle = styled.div`
 margin : 0 auto;
 padding : 1 rem;
 width: 500px;
 background-color: #141414;
 border-radius: 5px;
+box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+h1{
+    color: #ddd;
+    text-align: center;
+    margin-bottom: 2rem;
+}
+form{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1.5rem;
+}
+input{
+    padding: 0.5rem;
+    background: #ddd;
+    border: none;
+    boder-radius: 5px 0 0 5px;
+    width: 80%;
+    transition: all 0.3s ease-in-out;
+    &:hover{
+        background: #fff;
+        outline: none;
+    }
+    &:active{
+        background: #fff;
+        outline: none;
+    }
+    &:focus{
+        background: #fff;
+        outline: none;
+    }
+}
+button{
+    border: none;
+    padding: 0.5rem 1rem;
+    border-radius: 0px 5px 5px 0px;
+    background: ${primaryColor};
+    color: #141414;
+    font-weight: 600;
+    cursor: pointer;
+}
 `;
 
 export default function TodoList(){
@@ -25,9 +68,7 @@ export default function TodoList(){
             </form>
 
             <ul>
-                <li>task1</li>
-                <li>task2</li>
-                <li>task3</li>
+                <TodoItem/>
             </ul>
         </TodoListStyle>
     )
